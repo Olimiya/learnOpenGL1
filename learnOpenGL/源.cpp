8ca79ca209e7652d6719d3f987ef197f63a2f2e8
JunhuiLee <861921648@qq.com>
2019-12-20 11:03:11 +0800

    提取一些设置函数，可自由设计变更；限定不能飞行（y=0)

--- learnOpenGL/源.cpp
+++ learnOpenGL/源.cpp
@@ -16,6 +16,8 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 GLFWwindow* init();
+void drawPointLight(Shader shader);
+void setDefaultLight(Shader shader);
 
 #define NR_POINT_LIGHTS 4
 
@@ -37,11 +39,56 @@ float lastFrame = 0.0f;
 
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 glm::vec3 pointLightPositions[] = {
-	glm::vec3(0.7f,  0.2f,  2.0f),
-	glm::vec3(2.3f, -3.3f, -4.0f),
-	glm::vec3(-4.0f,  2.0f, -12.0f),
-	glm::vec3(0.0f,  0.0f, -3.0f)
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(1.0f, 1.0f, 1.0f),
+	glm::vec3(-1.0f,  -1.0f, -1.0f),
+	glm::vec3(2.0f,  2.0f, 2.0f)
 };
+float vertices[] = {
+	// positions          // normals           // texture coords
+	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+};
+unsigned int lightVAO;
 
 int main()
 {
@@ -51,11 +98,20 @@ int main()
 
 	Shader ourShader("vshader_model.glsl", "fshader_model.glsl");
 
-	// load models
-	// -----------
+	Shader lampShader("vshader_light.glsl", "fshader_light.glsl");
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 	Model ourModel("./obj/Space Station Scene.obj");
-	//Model ourModel1("./nanosuit/nanosuit.obj");
-	//Model ourModel2("./2/The City.obj");
+	Model ourModel1("./nanosuit/nanosuit.obj");
 
 	SceneName scene = Olimi;
 
@@ -75,40 +131,7 @@ int main()
 		{
 			ourShader.use();
 			//设置光源
-			//定向光
-			ourShader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
-			ourShader.setVec3("dirLight.diffuse", 1.0f, 1.0f, 1.0f);
-			ourShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
-			ourShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
-			//点光源
-			for (int i = 0; i < NR_POINT_LIGHTS; i++)
-			{
-				std::string name = "pointLights[";
-				std::string temp;
-				std::stringstream ss;
-				ss << i;
-				ss >> temp;
-				name = name + temp + ("].");
-				ourShader.setVec3(name + "pos", pointLightPositions[i]);
-				ourShader.setVec3(name + "ambient", 0.05f, 0.05f, 0.05f);
-				ourShader.setVec3(name + "diffuse", 0.8f, 0.8f, 0.8f);
-				ourShader.setVec3(name + "specular", 1.0f, 1.0f, 1.0f);
-				ourShader.setFloat(name + "constant", 1.0f);
-				ourShader.setFloat(name + "linear", 0.09);
-				ourShader.setFloat(name + "quadratic", 0.032);
-			}
-			//聚光灯
-			ourShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
-			ourShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
-			ourShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
-			ourShader.setFloat("spotLight.constant", 1.0f);
-			ourShader.setFloat("spotLight.linear", 0.09);
-			ourShader.setFloat("spotLight.quadratic", 0.032);
-			ourShader.setVec3("spotLight.pos", camera.Position);
-			ourShader.setVec3("spotLight.direction", camera.Front);
-			ourShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
-			ourShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
-
+			setDefaultLight(ourShader);
 			ourShader.setVec3("viewPos", camera.Position);
 
 			// view/projection transformations
@@ -124,8 +147,11 @@ int main()
 			model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
 			ourShader.setMat4("model", model);
 			ourModel.Draw(ourShader);
+			ourModel1.Draw(ourShader);
+
+			drawPointLight(lampShader);
 		}
-			break;
+		break;
 		case SICONG:
 			break;
 		case HUANG:
@@ -185,6 +211,69 @@ GLFWwindow * init()
 		exit(-1);
 	}
 	return window;
+}
+
+void drawPointLight(Shader shader)
+{
+	shader.use();
+	glm::mat4 projection = glm::perspective(
+		glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+	glm::mat4 view = camera.GetViewMatrix();
+	shader.setMat4("projection", projection);
+	shader.setMat4("view", view);
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::translate(model, lightPos);
+	model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+	shader.setMat4("model", model);
+	glBindVertexArray(lightVAO);
+	for (unsigned int i = 0; i < NR_POINT_LIGHTS; i++)
+	{
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, pointLightPositions[i]);
+		model = glm::scale(model, glm::vec3(0.2f)); // Make it a smaller cube
+		shader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
+}
+
+void setDefaultLight(Shader shader)
+{
+	//定向光
+	shader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
+	shader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+	shader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+	shader.setVec3("dirLight.direction", -0.2f, -1.0f, 0.0f);
+	//点光源
+	for (int i = 0; i < NR_POINT_LIGHTS; i++)
+	{
+		std::string name = "pointLights[";
+		std::string temp;
+		std::stringstream ss;
+		ss << i;
+		ss >> temp;
+		name = name + temp + ("].");
+		if (i == 0)
+			pointLightPositions[i] =
+			glm::vec3(camera.Position.x, camera.Position.y + 1.0f, camera.Position.z);
+		shader.setVec3(name + "pos", pointLightPositions[i]);
+		shader.setVec3(name + "ambient", 0.05f, 0.05f, 0.05f);
+		shader.setVec3(name + "diffuse", 0.8f, 0.8f, 0.8f);
+		shader.setVec3(name + "specular", 1.0f, 1.0f, 1.0f);
+		shader.setFloat(name + "constant", 1.0f);
+		shader.setFloat(name + "linear", 0.09);
+		shader.setFloat(name + "quadratic", 0.032);
+	}
+	//聚光灯
+	shader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
+	shader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+	shader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+	shader.setFloat("spotLight.constant", 1.0f);
+	shader.setFloat("spotLight.linear", 0.09);
+	shader.setFloat("spotLight.quadratic", 0.032);
+	shader.setVec3("spotLight.pos", camera.Position);
+	shader.setVec3("spotLight.direction", camera.Front);
+	shader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
+	shader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
